a4e5a6a59d0123d5866434dba55985458bfe4c18
BetterCallAle <dap22091998@gmail.com>
2023-01-10 17:11:27 +0100

    added the delete button in the show page

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -5,11 +5,19 @@
 @section('content')
     <div class="container mt-5">
         <h2 class="text-center mb-3">{{ $project->title }}</h2>
-        <div class="controls d-flex justify-content-between">
+        <div class="d-flex justify-content-between">
             <small>Craeto il {{ $project->created_at }}</small>
-            <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-secondary">
-                <i class="fa-solid fa-pencil"></i>
-            </a>
+            <div class="controls">
+                <a href="{{ route('admin.projects.edit', $project->slug) }}" class="btn btn-secondary">
+                    <i class="fa-solid fa-pencil"></i>
+                </a>
+
+                <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST" class="d-inline">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
+                </form>
+            </div>
         </div>
         <p class="mt-4">{{ $project->description }}</p>
     </div>
